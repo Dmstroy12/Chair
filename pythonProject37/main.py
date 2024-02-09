e7f4124b9637f9347fdc3bb5a5b34a3603983af5
Dmstroy12 <159549162+Dmstroy12@users.noreply.github.com>
2024-02-09 17:03:37 +0300

    Update main.py

--- pythonProject37/main.py
+++ pythonProject37/main.py
@@ -3,12 +3,15 @@ class Chair:
         self.countlegs = countlegs
         self.material = material
         self.color = color
+        
     def corect_material(self, text):
         return self.material == text
+        
     def __str__(self):
         return f"Chair({self.countlegs}), material({self.material}) , color({self.color})."
+        
+    def __repr__(self):
+        return str(self.countlegs)
 
 a = Chair(4, 'wood', 'Nutty')
 print(a)
-
-
